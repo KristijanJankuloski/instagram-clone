d6f2599962afc8c1ab8570d9bf8cb4a01eb05447
Kristijan <jankuloski.k@gmail.com>
2023-10-16 15:25:53 +0200

    moved photos routes to child routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,18 +3,24 @@ import { HomeComponent } from './core/components/home/home.component';
 
 export const routes: Routes = [
         { path: 'home', component: HomeComponent },
-        { path: 'photos', loadComponent: () => 
-                import('./features/photos/photo-list/photo-list.component')
-                .then(c => c.PhotoListComponent) 
-        },
-        { path: 'photo/edit', loadComponent: () => 
-                import('./features/photos/photo-edit/photo-edit.component')
-                .then(c => c.PhotoEditComponent) 
-        },
-        { path: 'photos/:id', loadComponent: () => 
-                import('./features/photos/photo-details/photo-details.component')
-                .then(c => c.PhotoDetailsComponent)
-        },
+        {path: 'photos', children: [
+                { path: '', loadComponent: () => 
+                        import('./features/photos/photo-list/photo-list.component')
+                        .then(c => c.PhotoListComponent) 
+                },
+                { path: 'create', loadComponent: () =>
+                        import('./features/photos/photo-create/photo-create.component')
+                        .then(c => c.PhotoCreateComponent) 
+                },
+                { path: 'edit', loadComponent: () => 
+                        import('./features/photos/photo-edit/photo-edit.component')
+                        .then(c => c.PhotoEditComponent) 
+                },
+                { path: ':id', loadComponent: () => 
+                        import('./features/photos/photo-details/photo-details.component')
+                        .then(c => c.PhotoDetailsComponent)
+                }
+        ]},
         { path: 'albums', loadComponent: () => 
                 import('./features/albums/albums-list/albums-list.component')
                 .then(c => c.AlbumsListComponent)
--- src/app/features/photos/photo-edit/photo-edit.component.ts
+++ src/app/features/photos/photo-edit/photo-edit.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PhotosService } from 'src/app/core/services/photos.service';
+import { MatCardModule } from '@angular/material/card';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ActivatedRoute } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-photo-edit',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
   templateUrl: './photo-edit.component.html',
   styleUrls: ['./photo-edit.component.scss']
 })
-export class PhotoEditComponent {
+export class PhotoEditComponent implements OnInit {
+  photoDetails = inject(PhotosService).photoDetails$.value;
+  route = inject(ActivatedRoute);
+  inputForm: FormGroup;
 
+  ngOnInit(): void {
+
+    this.initForm();
+  }
+
+  initForm() {
+    this.inputForm = new FormGroup({
+      title: new FormControl(this.photoDetails.title, [Validators.required, Validators.minLength(5)])
+    });
+  }
 }
